2612848b7e9665fd6ce5a28bcf72aa37568a9757
Gediminas Melinauskas <g.melinauskas0895@gmail.com>
2022-10-06 16:42:05 +0100

    added edditing to reviews

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -7,13 +7,20 @@ function FeedbackList() {
 
     const {feedbackData} = useContext(FeedbackAppContext);
 
-    return (
-        <div>
-            {feedbackData.map((feedbackItem) => {
-                return <FeedbackReview key={feedbackItem.id} feedbackItem={feedbackItem}/>;
-            })}
-        </div>
-    )
+    if (feedbackData.length === 0) {
+        return (
+            <p>No Data. Refresh</p>
+        )
+    } else {
+
+        return (
+            <div>
+                {feedbackData.map((feedbackItem) => {
+                    return <FeedbackReview key={feedbackItem.id} feedbackItem={feedbackItem}/>;
+                })}
+            </div>
+        )
+    }
 
 }
 
